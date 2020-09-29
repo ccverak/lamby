11ab080ba11db9ef119f6ba6a60658583ca926bb
Carlos Castellanos Vera <ccverak@gmail.com>
2020-09-30 00:14:47 +0200

    Update railtie.rb

--- lib/lamby/railtie.rb
+++ lib/lamby/railtie.rb
@@ -2,11 +2,5 @@ module Lamby
   class Railtie < ::Rails::Railtie
 
     config.lamby = ActiveSupport::OrderedOptions.new
-
-    rake_tasks do
-      load 'lamby/tasks.rake'
-      load 'lamby/templates.rake'
-    end
-
   end
 end
